93e3a2ed4b1a09a5536783f04d7a42ff2608e4dd
Pranav Vasan <pranavvasan@Pranavs-MacBook-Air.local>
2023-04-19 20:26:32 -0700

    about us

---
+++ about_us/about_us.js
@@ -0,0 +1,38 @@
+// Get the button element
+var aboutButton = document.getElementById("aboutButton");
+
+// Add click event listener to the button
+aboutButton.addEventListener("click", function() {
+  // Navigate to the desired page
+  window.location = 'about_us.html';
+});
+
+
+// Get the button element
+var homeButton = document.getElementById("homeButton");
+
+// Add click event listener to the button
+homeButton.addEventListener("click", function() {
+  // Navigate to the desired page
+  window.location.href = "../home/home.html";
+});
+
+
+// Get the button element
+var surveyButton = document.getElementById("surveyButton");
+
+// Add click event listener to the button
+surveyButton.addEventListener("click", function() {
+  // Navigate to the desired page
+  window.location.href = "../survey/survey.html";
+});
+
+
+// Get the button element
+var resultsButton = document.getElementById("resultsButton");
+
+// Add click event listener to the button
+resultsButton.addEventListener("click", function() {
+  // Navigate to the desired page
+  window.location.href = "../guide/guide.html";
+});
