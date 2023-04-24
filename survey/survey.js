e93eb804b156601879e33abc1992f02c42a042f2
Pranav Vasan <pranavvasan@Pranavs-MacBook-Air-3.local>
2023-04-23 17:50:15 -0700

    survey page

--- survey/survey.js
+++ survey/survey.js
@@ -46,3 +46,320 @@ continueButton.addEventListener("click", function() {
   // Navigate to the desired page
   window.location.href = "../guide/guide.html";
 });
+
+
+var sportsButton = document.getElementById("sportsButton");
+var isClicked = false; // Store the clicked state
+
+sportsButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    sportsButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    sportsButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    sportsButton.style.backgroundColor = randomColor;
+    sportsButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var writingButton = document.getElementById("writingButton");
+var isClicked = false; // Store the clicked state
+
+writingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    writingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    writingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    writingButton.style.backgroundColor = randomColor;
+    writingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+
+var eatingButton = document.getElementById("eatingButton");
+var isClicked = false; // Store the clicked state
+
+eatingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    eatingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    eatingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    eatingButton.style.backgroundColor = randomColor;
+    eatingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+
+var skiingButton = document.getElementById("skiingButton");
+var isClicked = false; // Store the clicked state
+
+skiingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    skiingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    skiingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    skiingButton.style.backgroundColor = randomColor;
+    skiingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var sleepingButton = document.getElementById("sleepingButton");
+var isClicked = false; // Store the clicked state
+
+sleepingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    sleepingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    sleepingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    sleepingButton.style.backgroundColor = randomColor;
+    sleepingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var makeupButton = document.getElementById("makeupButton");
+var MakeupisClicked = false; // Store the clicked state
+
+makeupButton.addEventListener("click", function() {
+  if (MakeupisClicked) {
+    // Revert to the original color
+    makeupButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    makeupButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    makeupButton.style.backgroundColor = randomColor;
+    makeupButton.style.color = "black";
+  }
+  MakeupisClicked = !MakeupisClicked; // Toggle the clicked state
+});
+
+var bakingButton = document.getElementById("bakingButton");
+var isClicked = false; // Store the clicked state
+
+bakingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    bakingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    bakingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    bakingButton.style.backgroundColor = randomColor;
+    bakingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var moviesButton = document.getElementById("moviesButton");
+var isClicked = false; // Store the clicked state
+
+moviesButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    moviesButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    moviesButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    moviesButton.style.backgroundColor = randomColor;
+    moviesButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+
+var bikingButton = document.getElementById("bikingButton");
+var isClicked = false; // Store the clicked state
+
+bikingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    bikingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    bikingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    bikingButton.style.backgroundColor = randomColor;
+    bikingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var fashionButton = document.getElementById("fashionButton");
+var isClicked = false; // Store the clicked state
+
+fashionButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    fashionButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    fashionButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    fashionButton.style.backgroundColor = randomColor;
+    fashionButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var liftingButton = document.getElementById("liftingButton");
+var isClicked = false; // Store the clicked state
+
+liftingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    liftingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    liftingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    liftingButton.style.backgroundColor = randomColor;
+    liftingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var runningButton = document.getElementById("runningButton");
+var isClicked = false; // Store the clicked state
+
+runningButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    runningButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    runningButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    runningButton.style.backgroundColor = randomColor;
+    runningButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var basketballButton = document.getElementById("basketballButton");
+var isClicked = false; // Store the clicked state
+
+basketballButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    basketballButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    basketballButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    basketballButton.style.backgroundColor = randomColor;
+    basketballButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var gamingButton = document.getElementById("gamingButton");
+var isClicked = false; // Store the clicked state
+
+gamingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    gamingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    gamingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    gamingButton.style.backgroundColor = randomColor;
+    gamingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+var cookingButton = document.getElementById("cookingButton");
+var isClicked = false; // Store the clicked state
+
+cookingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    cookingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    cookingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    cookingButton.style.backgroundColor = randomColor;
+    cookingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+
+var gardeningButton = document.getElementById("gardeningButton");
+var isClicked = false; // Store the clicked state
+
+gardeningButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    gardeningButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    gardeningButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    gardeningButton.style.backgroundColor = randomColor;
+    gardeningButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+
+var drawingButton = document.getElementById("drawingButton");
+var isClicked = false; // Store the clicked state
+
+drawingButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    drawingButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    drawingButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    drawingButton.style.backgroundColor = randomColor;
+    drawingButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+
+var musicButton = document.getElementById("musicButton");
+var isClicked = false; // Store the clicked state
+
+musicButton.addEventListener("click", function() {
+  if (isClicked) {
+    // Revert to the original color
+    musicButton.style.backgroundColor = "rgba(124,174,76,0.28999999165534973)";
+    musicButton.style.color = "black";
+  } else {
+    // Change to a new color
+    var randomColor = "#4caf50";
+    musicButton.style.backgroundColor = randomColor;
+    musicButton.style.color = "black";
+  }
+  isClicked = !isClicked; // Toggle the clicked state
+});
+
+
+
+
