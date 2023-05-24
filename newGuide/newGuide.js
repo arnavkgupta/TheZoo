f9bc0b357a9f6dfe6900ff456b5a8c1fa454e6c9
Pranav Vasan <pranavvasan@Pranavs-MacBook-Air-3.local>
2023-05-23 18:27:34 -0700

    guide

--- newGuide/newGuide.js
+++ newGuide/newGuide.js
@@ -37,50 +37,74 @@ resultsButton.addEventListener("click", function() {
   window.location.href = "../newGuide/newGuide.html";
 });
 
-// Function to filter the items
-function filterItems() {
-  // Get the value entered in the input
-  const filterValue = filterInput.value.toLowerCase();
-
-  // Get the selected checkboxes
-  const selectedCheckboxes = Array.from(checkboxes)
-    .filter(checkbox => checkbox.checked)
-    .map(checkbox => checkbox.value.toLowerCase());
-
-  // Get the items
-  const items = document.querySelectorAll('.item');
-
-  // Loop through each item
-  items.forEach(item => {
-    const category = item.getAttribute('data-category').toLowerCase();
-
-    // Check if the item matches the filter value and selected checkboxes
-    const matchFilter = filterValue === '' || item.innerText.toLowerCase().includes(filterValue);
-    const matchCategory = selectedCheckboxes.length === 0 || selectedCheckboxes.includes(category);
-
-    // Show or hide the item based on filter and checkbox matches
-    if (matchFilter && matchCategory) {
-      item.style.display = ''; // Show the item
-    } else {
-      item.style.display = 'none'; // Hide the item
-    }
-  });
-}
-
 // Function to flip the card
 function flipCard(index) {
   const card = document.getElementsByClassName('item')[index];
   card.classList.toggle('flipped');
 }
 
-// Get the filter input and checkboxes
-const filterInput = document.getElementById('filterInput');
-const checkboxes = document.querySelectorAll('.filter-checkbox');
+// Scroll to the top of the page on load/refresh
+window.addEventListener('load', () => {
+  // Scroll to the top with smooth behavior
+  window.scrollTo({ top: 0, behavior: 'auto' });
+});
 
-// Attach event listeners
-filterInput.addEventListener('input', filterItems);
-checkboxes.forEach(checkbox => checkbox.addEventListener('change', filterItems));
+var artButton = document.getElementById("artButton");
+artButton.addEventListener("click", function() {
+  window.scrollTo({top: 490});
+});
 
-// Call filterItems() initially
-filterItems();
+var runningButton = document.getElementById("runningButton");
+runningButton.addEventListener("click", function() {
+  window.scrollTo({top: 1283});
+});
 
+var liftingButton = document.getElementById("liftingButton");
+liftingButton.addEventListener("click", function() {
+  window.scrollTo({top: 2073});
+});
+
+var readingButton = document.getElementById("readingButton");
+readingButton.addEventListener("click", function() {
+  window.scrollTo({top: 2470});
+});
+
+var musicButton = document.getElementById("musicButton");
+musicButton.addEventListener("click", function() {
+  window.scrollTo({top: 2869});
+});
+
+var makeupButton = document.getElementById("makeupButton");
+makeupButton.addEventListener("click", function() {
+  window.scrollTo({top: 3265});
+});
+
+var fashionButton = document.getElementById("fashionButton");
+fashionButton.addEventListener("click", function() {
+  window.scrollTo({top: 4058});
+});
+
+var skiingButton = document.getElementById("skiingButton");
+skiingButton.addEventListener("click", function() {
+  window.scrollTo({top: 4850});
+});
+
+var foodButton = document.getElementById("foodButton");
+foodButton.addEventListener("click", function() {
+  window.scrollTo({top: 5246});
+});
+
+var hikingButton = document.getElementById("hikingButton");
+hikingButton.addEventListener("click", function() {
+  window.scrollTo({top: 6434});
+});
+
+var paButton = document.getElementById("paButton");
+paButton.addEventListener("click", function() {
+  window.scrollTo({top: 7226});
+});
+
+var sportsButton = document.getElementById("sportsButton");
+sportsButton.addEventListener("click", function() {
+  window.scrollTo({top: 8018});
+});
